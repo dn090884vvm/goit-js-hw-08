86125700c0f06e9cb3c88948e93a65b38dd2028f
Vladimir Vashchenko <dn090884vvm@gmail.com>
2022-05-23 15:15:27 +0300

    Making some bullshit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const imagesMarkup = createImegaesCardsMarkup(galleryItems);
+
+function createImegaesCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li><a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a></li>`;
+    })
+    .join('');
+}
+
+galleryContainer.insertAdjacentHTML('beforeend', imagesMarkup);
+
+const modalWin = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
